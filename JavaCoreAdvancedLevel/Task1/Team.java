ca43afaf39a4bf615504697d0869b9eced6297bb
petrovichpas <35486297+petrovichpas@users.noreply.github.com>
2019-09-18 23:52:34 +0300

    Update Team.java

--- JavaCoreAdvancedLevel/Task1/Team.java
+++ JavaCoreAdvancedLevel/Task1/Team.java
@@ -1,35 +1,21 @@
 package JavaCoreAdvancedLevel.Task1;
 
-import JavaCoreAdvancedLevel.Task1.competitor.Cat;
-import JavaCoreAdvancedLevel.Task1.competitor.Dog;
-import JavaCoreAdvancedLevel.Task1.competitor.Human;
+import JavaCoreAdvancedLevel.Task1.competitor.Competitor;
 
 public class Team {
-    private Human human;
-    private Cat cat;
-    private Dog dog;
+    private Competitor[] competitors;
 
-    public Team(Human h, Cat c, Dog d) {
-        this.human = h;
-        this.cat = c;
-        this.dog = d;
+    public Team(Competitor... competitor) {
+        this.competitors = competitor;
     }
 
-    public Human getHuman() {
-        return human;
-    }
-
-    public Cat getCat() {
-        return cat;
-    }
-
-    public Dog getDog() {
-        return dog;
+    public Competitor[] getCompetitors() {
+        return competitors;
     }
 
     public void showResults(){
-        getHuman().info();
-        getCat().info();
-        getDog().info();
+        for (Competitor c : competitors){
+            c.info();
+        }
     }
 }
