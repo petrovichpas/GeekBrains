09683fa438587515af24763e38ed27f0eeb61d1d
petrovichpas <35486297+petrovichpas@users.noreply.github.com>
2019-09-22 14:58:24 +0300

    Update MyArray.java

--- JavaCoreAdvancedLevel/Task2/MyArray.java
+++ JavaCoreAdvancedLevel/Task2/MyArray.java
@@ -4,9 +4,6 @@ public class MyArray {
 
     public static void main(String[] args) {
         try {
-            // для проверки
-//            System.out.println(getSumArr(new String[][] {{"1", "2", "3", "4", "5"}, {"6", "7", "8", "9"}}));
-//            System.out.println(getSumArr(new String[][] {{"-1", "Х", "3", "4"}, {"6", "7", "8", "9"}}));
             System.out.println(getSumArr(new String[][] {{"-1", "2", "3", "4"}, {"6", "7", "8", "9"}}));
         } catch (MyArraySizeException e) {
             e.printStackTrace();
@@ -18,7 +15,7 @@ public class MyArray {
     public static int getSumArr(String[][] arr) throws MyArraySizeException, MyArrayDataException {
         int sum = 0;
 
-        if (arr[0].length + arr[1].length == 8){
+        if (arr[0].length == 4 && arr[1].length == 4){
             for (int i = 0; i < arr.length; i++){
                 for (int j = 0; j < arr[1].length; j++){
                     try {
