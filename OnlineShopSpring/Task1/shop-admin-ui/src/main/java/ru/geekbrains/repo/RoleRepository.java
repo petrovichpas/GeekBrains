e7ce8c35477fd1d2fff31d19a23d060e2d7b89a8
petrovichpas <35486297+petrovichpas@users.noreply.github.com>
2020-08-03 13:35:16 +0300

    Delete RoleRepository.java

--- OnlineShopSpring/Task1/shop-admin-ui/src/main/java/ru/geekbrains/repo/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.geekbrains.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import ru.geekbrains.entity.Role;
-
-@Repository
-public interface RoleRepository extends JpaRepository<Role, Long> {
-}
